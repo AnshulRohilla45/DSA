f5e044dd400d730da7f41d7f14ad31ec9b4cb264
AnshulRohilla45 <anshulrohilla45@gmail.com>
2023-06-09 22:29:41 +0530

    Added largestarray

---
+++ largestarray.java
@@ -0,0 +1,14 @@
+public class largestarray {
+    public static void main(String[] args){
+        int[] arr = new int[]{10,15,30,55};
+        int max = arr[0];
+        for(int i=0; i<max; i++){
+            if(arr[i]>max){
+                max = arr[i];
+
+    }
+    System.out.println("largest is " + max);
+    
+}
+}
+}
